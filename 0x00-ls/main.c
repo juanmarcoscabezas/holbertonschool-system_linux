3300aaccb570359a746fc7c3d34d0c5a54415ea5
juanmarcos <juanmarcoscabezas@gmail.com>
2020-07-13 19:14:56 -0500

    Created makefile

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+void execute(int, char **);
 
 /**
  * main - Entry point
- * @void - Application parameter
+ * @void: Application parameter
  * Return: Always 0, -1 otherwise
  */
-int main(void)
+int main(int argc, char **argv)
 {
+	execute(argc, argv);
 	return (0);
+}
+
+void execute(int argc, char **argv)
+{
+	DIR *dir;
+	struct dirent *read;
+
+	if (argc <= 1)
+	{
+		dir = opendir(".");
+		while ((read = readdir(dir)) != NULL)
+		{
+			printf("%s\n", read->d_name);
+		}
+	}
+	else
+	{
+		dir = opendir(argv[1]);
+		while ((read = readdir(dir)) != NULL)
+		{
+			printf("%s\n", read->d_name);
+		}
+	}
+	closedir(dir);
 }
