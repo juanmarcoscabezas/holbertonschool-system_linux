48c1e26a9b123c2d683bcac763feb14dfb1c9af7
juanmarcos <juanmarcoscabezas@gmail.com>
2020-07-17 11:41:14 -0500

    Testing if is a typo

--- 0x00-ls/lib.c
+++ 0x00-ls/lib.c
@@ -30,8 +30,8 @@ char *error_message(int error)
 {
 	char *error_messages[3] = {
 		"",
-		"cannot access",
-		"cannot open directory"};
+		"cannot acces",
+		"cannot open director"};
 
 	switch (error)
 	{
