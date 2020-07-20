3ec5a0e22de13a2b2cd1636ab1ea0016db723865
juanmarcos <juanmarcoscabezas@gmail.com>
2020-07-19 19:18:51 -0500

    Solved seg fault

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -24,10 +24,11 @@ int execute(int argc, char **argv)
 	ls_struct = initialize_ls(ls_struct);
 	ls_struct.options = get_options_list(argc, argv, ls_struct.options);
 	ls_struct = get_arguments(argc, argv, ls_struct);
-	ls_struct.directories = sort_directories_list_by_name(
-		ls_struct.directories,
-		ls_struct.directories_number
-	);
+	if (ls_struct.directories_number > 1)
+		ls_struct.directories = sort_directories_list_by_name(
+			ls_struct.directories,
+			ls_struct.directories_number
+		);
 
 	print_errors_access(ls_struct);
 	print_files(ls_struct);
